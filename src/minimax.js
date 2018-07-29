d7c038f99f6768f5717ee6b82ca32e9b505bb1e2
Michael Tauer <michael.tauer@futurice.com>
2018-07-29 11:14:22 +0200

    Use options parameter for minimax function

--- src/minimax.js
+++ src/minimax.js
@@ -2,31 +2,39 @@ import minBy from 'lodash/minBy';
 import maxBy from 'lodash/maxBy';
 import meanBy from 'lodash/meanBy';
 
-const suboptimalWeight = 0.05;
+const defaultOptions = {
+  suboptimalWeight: 0.05,
+}
 
-export default function minimax(game, state, maximize = true, maxDepth = 9) {
+export default function minimax(game, state, maximize = true, maxDepth = 9, options = defaultOptions) {
   const actions = game.getValidActions(state);
   return actions
     .map(action => {
       const nextState = game.performAction(state, action);
       const nextValue = game.getValue(nextState);
-      let deepMinimax = { value: 0, states: [] };
-      let suboptimalValue = 0;
+      let optimalDeepStates = [];
+      let optimalDeepValue = 0;
+      let suboptimalDeepValue = 0;
       if (maxDepth > 0 && !game.hasFinished(nextState)) {
-        const deepMinimaxItems = minimax(game, nextState, !maximize, maxDepth - 1);
-        deepMinimax = maximize ?
+        const deepMinimaxItems = minimax(game, nextState, !maximize, maxDepth - 1, options);
+        const deepMinimax = maximize ?
           minBy(deepMinimaxItems, o => o.value) :
           maxBy(deepMinimaxItems, o => o.value);
+        optimalDeepValue = deepMinimax.value;
+        optimalDeepStates = deepMinimax.states;
         if (maximize) {
           const suboptimalItems = deepMinimaxItems
             .filter(o => o.action !== deepMinimax.action);
-          suboptimalValue = meanBy(suboptimalItems, o => o.value) || 0;
+          suboptimalDeepValue = meanBy(suboptimalItems, o => o.value) || 0;
         }
       }
       return {
         action,
-        states: [ nextState, ...deepMinimax.states],
-        value: nextValue + (1 - suboptimalWeight) * deepMinimax.value + suboptimalWeight * suboptimalValue,
+        states: [ nextState, ...optimalDeepStates],
+        value:
+          nextValue +
+          (1 - options.suboptimalWeight) * optimalDeepValue +
+          options.suboptimalWeight * suboptimalDeepValue,
       };
     });
 }
